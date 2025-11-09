a4a4b619bfacc4bcecd58358dd659348539fd20a
Cameron Choong <cameronchoong17@gmail.com>
2025-11-09 12:54:27 -0500

    created class and initial put

--- approvalform.py
+++ approvalform.py
@@ -5,3 +5,11 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class Request(BaseModel):
+    projname: str
+    price: float
+    link: str
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Request):
+    return {"item_name": item.name, "item_id": item_id}
